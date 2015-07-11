7a09b52d6f5d0cf03c3bc2d5c1a0524a0917e3c7
Shawn Rice <iamshawnrice@gmail.com>
2015-07-11 10:18:44 -0400

    extract accordionToggle function out to the directive file

--- js/controllers/playlist-controller.js
+++ js/controllers/playlist-controller.js
@@ -9,17 +9,6 @@ define([
     playlistsFactory.getPlaylist($stateParams.slug).then(function(data) {
       $scope.playlist = data[0];
       $scope.playlist.published = moment($scope.playlist.date).format('MMMM Do YYYY');
-
-      $scope.accordionToggle = function(e) {
-        var $self = $(e.currentTarget),
-            $next = $self.next('.js-accordion-panel'),
-            $panels = $('.js-accordion-panel').not($next);
-
-        $panels.velocity('slideUp', { duration: 250 });
-        $next.velocity('slideDown', { duration: 250, delay: 250 });
-
-        e.preventDefault();
-      };
     });
   });
 });
--- js/directives/track-list.js
+++ js/directives/track-list.js
@@ -7,7 +7,19 @@ define([
   app.directive('iasrTrackList', function() {
     return {
       restrict: 'E',
-      templateUrl: 'partials/track-list.html'
+      templateUrl: 'partials/track-list.html',
+      link: function(scope) {
+        scope.accordionToggle = function(e) {
+          var $self = $(e.currentTarget),
+              $next = $self.next('.js-accordion-panel'),
+              $panels = $('.js-accordion-panel').not($next);
+
+          $panels.velocity('slideUp', { duration: 250 });
+          $next.velocity('slideDown', { duration: 250, delay: 250 });
+
+          e.preventDefault();
+        };
+      }
     };
   });
 });
