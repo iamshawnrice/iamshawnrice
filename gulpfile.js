82dc8137457572f45c5df6a5591475b0cd65dd2a
Shawn Rice <iamshawnrice@gmail.com>
2015-07-20 20:54:51 -0400

    Gulp serve & sass compile is complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+// Sass Tasks
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'))
+      .pipe(autoprefixer())
+      .pipe(browserSync.stream());
 });
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    proxy: "localhost:8888"
+  });
+
+  gulp.watch("./sass/**/*.scss", ['sass']);
+});
+
+gulp.task('default', ['serve']);
