e912697cc390285f825cb70beace3963b3ac8be3
Shawn Rice <iamshawnrice@gmail.com>
2015-07-20 21:22:59 -0400

    All CSS-related dev tasks are complete

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,16 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 // Sass Tasks
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+      .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest('./css'))
-      .pipe(autoprefixer())
       .pipe(browserSync.stream());
 });
 
