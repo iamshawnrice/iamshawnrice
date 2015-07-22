c00bf842918f3abae50f1b7622823c728a00400f
Shawn Rice <iamshawnrice@gmail.com>
2015-07-21 21:28:36 -0400

    WIP - replace requirejs and AngularAMD with gulp concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,34 @@ var gulp = require('gulp');
 
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
+
+
+// JS Tasks
+gulp.task('js', function() {
+  return gulp.src('./js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('js', function () {
+  gulp.src(['src/**/module.js', 'src/**/*.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('.'))
+});
+
 // Sass Tasks
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-      .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('./css'))
-      .pipe(browserSync.stream());
+  gulp.src('./sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
