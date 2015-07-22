7ee055adbbf2c84b1318c86d8b134d6f39837ee7
Shawn Rice <iamshawnrice@gmail.com>
2015-07-21 22:28:22 -0400

    gulp is now successfully concatenating JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
 var gulp = require('gulp');
 
 var autoprefixer = require('gulp-autoprefixer');
+var bowerFiles = require('main-bower-files');
 var browserSync = require('browser-sync').create();
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
-
-
 // JS Tasks
 gulp.task('js', function() {
-  return gulp.src('./js/*.js')
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./dist/'));
-});
+  var files = bowerFiles();
 
-gulp.task('js', function () {
-  gulp.src(['src/**/module.js', 'src/**/*.js'])
+  files.push('./js/**/*.js');
+
+  gulp.src(files)
+    .pipe(sourcemaps.init())
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('.'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.stream());
 });
 
 // Sass Tasks
@@ -35,10 +35,11 @@ gulp.task('sass', function () {
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
   browserSync.init({
-    proxy: "localhost:8888"
+    proxy: 'localhost:8888'
   });
 
-  gulp.watch("./sass/**/*.scss", ['sass']);
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./js/**/*.js', ['js']);
 });
 
 gulp.task('default', ['serve']);
