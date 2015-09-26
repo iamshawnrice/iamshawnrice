b51e1584cc4fef2296dbf526b6fd5080b99e4699
Shawn Rice <iamshawnrice@gmail.com>
2015-09-26 16:43:26 -0400

    check in compiled js

--- dist/scripts.js
+++ dist/scripts.js
@@ -53190,32 +53190,6 @@ will produce an inaccurate conversion value. The same issue exists with the cx/c
 (function() {
   'use strict';
 
-  angular.module('app.iasr').filter('customDateFilter', function() {
-    return {
-      verbal: function(date) {
-        var description = moment(date).format('DD'),
-            month = moment(date).format('MMMM'),
-            year = moment(date).format('YYYY');
-
-        function parseDay(description) {
-          if (description < 10) {
-            return 'early';
-          } else if (description > 10 && description < 21) {
-            return 'mid';
-          } else {
-            return 'late';
-          }
-        }
-
-        return parseDay(description) + month + year;
-      }
-    }
-  });
-})();
-
-(function() {
-  'use strict';
-
   angular.module('app.iasr').factory('dateService', function() {
     return {
       verbal: function(date) {
@@ -53304,6 +53278,32 @@ will produce an inaccurate conversion value. The same issue exists with the cx/c
     };
 
     return service;
+  });
+})();
+
+(function() {
+  'use strict';
+
+  angular.module('app.iasr').filter('customDateFilter', function() {
+    return {
+      verbal: function(date) {
+        var description = moment(date).format('DD'),
+            month = moment(date).format('MMMM'),
+            year = moment(date).format('YYYY');
+
+        function parseDay(description) {
+          if (description < 10) {
+            return 'early';
+          } else if (description > 10 && description < 21) {
+            return 'mid';
+          } else {
+            return 'late';
+          }
+        }
+
+        return parseDay(description) + month + year;
+      }
+    }
   });
 })();
 
